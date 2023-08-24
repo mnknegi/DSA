464e207f091b0d1c59a84e51c07f3f78c7fca685
imayanknegi29 <imayanknegi29@gmail.com>
2023-08-24 22:10:50 +0530

    pointers with array

--- Pointers/Pointers/Pointer.cpp
+++ Pointers/Pointers/Pointer.cpp
@@ -10,6 +10,8 @@
 
 using namespace::std;
 
+// pointer to a value
+/*
 int main() {
     int a = 10;
     int *p;
@@ -19,4 +21,20 @@ int main() {
     printf("using pointer %d", *p);
 
     return  0;;
+}
+*/
+
+// pointer to a array
+
+int main() {
+    int A[5] = {2,4,6,8,10};
+    int *p;
+    p = A;  // Array is itself a pointer.
+
+    for(int i = 0; i < 5; i++) {
+//        cout<<A[i]<<endl;
+        printf("%d \n", p[i]);
+    }
+
+    return  0;
 }
