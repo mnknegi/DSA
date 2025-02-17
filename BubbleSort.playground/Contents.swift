274a1c0e03923080afd65673e082dd18f41a0135
imayanknegi29 <imayanknegi29@gmail.com>
2025-02-17 16:16:06 +0530

    added condition when array is already sorted.

--- BubbleSort.playground/Contents.swift
+++ BubbleSort.playground/Contents.swift
@@ -3,14 +3,19 @@
 func bubbleSort(arr: [Int]) {
     var array = arr
     let size = array.count - 1
+    var swapped = false
     for index_i in stride(from: size, through: 1, by: -1) { // Loop through (array.count - 1) to 1
+        swapped = false
         for index_j in 0..<index_i {                        // Loop througn 0 to index from above.
             if array[index_j] > array[index_j+1] {
                 let temp = array[index_j]
                 array[index_j] = array[index_j+1]
                 array[index_j+1] = temp
+                swapped = true
             }
         }
+
+        if swapped == false { break } // if already sorted.
     }
     print(array)
 }
